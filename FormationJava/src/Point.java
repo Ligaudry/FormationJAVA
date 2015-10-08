5f3635c8ec22e90341eac3f598c9b175fdd9ea44
adminlocal <adminlocal@C6-PC.formation.local>
2015-10-08 15:56:19 +0200

    POO

---
+++ FormationJava/src/Point.java
@@ -0,0 +1,16 @@
+
+public class Point {
+
+	int x;
+	int y;
+	
+	public void deplacer(int newx, int newy){
+		x=newx;
+		y=newy;
+	}
+	
+	public void afficher(){
+		System.out.println("("+x+";"+y+")");
+	}
+}
+
